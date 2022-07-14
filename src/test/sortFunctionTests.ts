06e8d99389e4bfe33b0914fe3f0097d7742f5e96
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-07-14 13:54:34 +0800

    a little modify

--- src/test/sortFunctionTests.ts
+++ src/test/sortFunctionTests.ts
@@ -3,7 +3,4 @@ import { bubbleSort } from "../tools/sortFunctions";
 export function sortFunctionTest(): void {
   const arr: Array<number> = [11, 313, 21, -12, 131, 11, 11, 13131, 43523];
   bubbleSort<number>(arr);
-  console.log(arr.reverse());
-  // reverse 会就地改变数组
-  console.log(arr);
 }
