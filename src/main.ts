f1afa794bcd50ff8a1f0304b2313f622889f9376
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-07-15 15:27:32 +0800

    a little frivolous modify

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { basicDataTypeTest } from "../src/test/basicDataTypeTest";
 // import { eventLoopTest } from "./test/eventLoopTest";
 // import { sortFunctionTest } from "./test/sortFunctionTests";
 
+window.addEventListener("load", () => {
+  const app: HTMLElement | null = document.getElementById("app");
+  if (app) {
+    app.removeAttribute("data-cloak");
+  }
+});
+
 formateLogOutput(["✨", `you are running on ${process.env.NODE_ENV} mode!`]);
 formateLogOutput(["👋", `https://www.github.com/sorry-for-time`], [styleEnum.title, styleEnum.hint]);
 
