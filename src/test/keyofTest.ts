42486e20068b6e26a7eccf814cfa28faa492292c
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-05 15:41:18 +0800

    完善测试用例

--- src/test/keyofTest.ts
+++ src/test/keyofTest.ts
@@ -1,4 +1,36 @@
 /**
+ * @description 简单的测试 demo
+ * @class Demo
+ */
+class Demo {
+  private _name: string;
+  private _age: number;
+  public readonly sex: 0 | 1;
+
+  constructor(name: string, age: number, sex: 0 | 1) {
+    this._name = name;
+    this._age = age;
+    this.sex = sex;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(value: string) {
+    this._name = value;
+  }
+
+  public get age(): number {
+    return this._age;
+  }
+
+  public set age(value: number) {
+    this._age = value;
+  }
+}
+
+/**
  * @description keyof 关键字的使用回顾
  * @template T
  * @param {T} target
@@ -11,13 +43,14 @@ function outputObjValue<T extends object>(target: T, descriptor: keyof T): { des
   };
 }
 
+/**
+ * @description keyof 简单回顾测试用例
+ * @export
+ */
 export function keyofTest(): void {
-  const tmp = {
-    name: "Smith",
-    age: 23,
-  };
-
+  const tmp = new Demo("Wayne", 23, 1);
   console.log(outputObjValue(tmp, "name"));
   console.log(outputObjValue(tmp, "age"));
-  //   console.log(outputObjValue(tmp, "none")); // 不允许, 语法检查不通过
+  console.log(outputObjValue(tmp, "sex"));
+  // console.log(outputObjValue(tmp, "nothing")); // 语法检测不通过, 不存在 "nothing" 这个 key
 }
