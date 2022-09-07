e44a695b695a9e5cfa5811e3e7d6b9c4b8389925
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-07 13:05:58 +0800

    add debounce decorator

--- src/core/annotations.ts
+++ src/core/annotations.ts
@@ -1,3 +1,5 @@
+import { debounce } from "src/util/performance";
+
 interface InjectFn {
   name: string;
   callBack: Function;
@@ -12,8 +14,20 @@ export function inject(...fns: Array<InjectFn>): ClassDecorator {
   return (target: Function) => {
     fns.forEach((item) => {
       console.log("===>", item);
-
       Reflect.set(target, item.name, item.callBack);
     });
+  };
+}
+
+/**
+ * @description 方法防抖节流装饰器
+ * @export
+ * @param {number} [delay=300] 防抖间隔延迟
+ * @param {boolean} [startImmediate=true] 第一次使用函数时是否立即执行, 无需等待
+ * @return {*}  {MethodDecorator}
+ */
+export function useDebounce(delay: number = 300, startImmediate: boolean = true): MethodDecorator {
+  return (target: Object, propertyKey: string | symbol, operationObject) => {
+    (operationObject.value as any) = debounce(target.constructor.prototype[propertyKey] as any, delay, startImmediate);
   };
 }
